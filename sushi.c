d8ac12d8ac01169e794da67913cb28806bb9935c
WDC <ISA@chengalexdeMacBook-Pro.local>
2016-01-20 11:47:35 +0800

    checkOccupied() become the most important function with recursive and iterative function the direction move might be a little bit redundant fix it later

--- sushi.c
+++ sushi.c
@@ -1,6 +1,3 @@
-//TODO need occupied bit of each block
-//TODO need new moving function
-
 #include<stdlib.h>
 #include<stdio.h>
 
@@ -9,19 +6,15 @@
 #define up 3
 #define down 4
 
-//void down(int,int,int,int,int,int);
-//void up(int,int,int,int,int,int);
-//void left(int,int,int,int,int,int);
-//void right(int,int,int,int,int,int);
 void sushi(int,int);
-void moveArray(int);
+int maintainQueue(int);
+void move();
+void checkOccupied();
 
-struct structure {
-    int count;
-    int occupied;
-};
-struct structure region[100][100];
+int** region=0;
 int* dir;
+int count=1;
+int x=0,y=0;
 
 int main(int argc,char* argv[]){
 	
@@ -32,18 +25,18 @@ int main(int argc,char* argv[]){
 
     int m=atoi(argv[1]);
 	int n=atoi(argv[2]);
-	int x=0,y=0,i=0;
+	int i=0,j=0;
     
-//    region = (int**)malloc(sizeof(int*)*m);
-//    for(i=0;i<m;i++)
-//        region[i] = (int*)malloc(m * n * sizeof(int)*n);
-    
+    region = (int**)malloc(sizeof(int*)*m);
+    for(i=0;i<m;i++)
+        region[i] = (int*)malloc(m * n * sizeof(int)*n);
 
 	sushi(m,n);
 
-	for(y=0;y<n;y++){
-		for(x=0;x<m;x++){
-			printf("%d,",region[x][y].count);
+    //TODO animize it
+	for(j=0;j<n;j++){//print results
+		for(i=0;i<m;i++){
+			printf("%d,",region[x][y]);
 		}
 		printf("\n");
 	}
@@ -51,163 +44,108 @@ int main(int argc,char* argv[]){
 }
 
 void sushi(int m, int n){
-	//printf("%d,%d\n",i,j);
-	int x=0,y=0,dir=0,count=2;
-	region[x][y].count=1;
-    for(int i=0;i<100;i++){
-        for(int j=0;j<100;j++){
-            region[i][j].occupied=0;
-            region[i][j].count=0;
+	//printf("%d,%d\n",i,j);    region[x][y]=1;
+    int i;
+    dir[0]=right; dir[1]=down; dir[2]=left;//queue of downward
+    while(y<n)
+        checkOccupied();
+    if(n%2=1){//if y is an odd number
+        while(x<m){
+            if(x%4==2 && y==n){
+                dir[0]=right; dir[1]=up; dir[2]=left;//queue of upward
+                while(y>0)
+                    checkOccupied();
+            }else if(x%4==0 && y==0){
+                dir[0]=right; dir[1]=down; dir[2]=left;//queue of downward
+                while(y<n)
+                    checkOccupied();
+            }else printf("error 1");
+            
         }
+    }else{
+        
     }
-	
+        
+    dir[0]=up; dir[1]=right; dir[2]=down;//queue of rightward
+    while(y<m)
+        checkOccupied();
+
 }
 
-void moveArray(int position){
+//TODO how to end (use count?)
+//TODO how to set key(odd/even)
+void checkOccupied(){
+    int i,noWay=0;
+    for(i=0;i<3;i++){
+        if(dir[i]=left){
+            if(region[x-1][y]=0)
+                x--;
+                move();
+            if(maintainQueue(i) && key){//change line & reverse the path(odd)
+                dir[0]=up; dir[1]=right; dir[2]=down;//queue of rightward
+                checkOccupied();
+            }else if(maintainQueue(i)){//change line after waste 2s(even)
+                dir[0]=right; dir[1]=down; dir[2]=left;//queue of downward
+                checkOccupied();
+            }
+        }else if(dir[i]=right){
+            if(region[x+1][y]=0){
+                x++;
+                move();
+                if(maintainQueue(i) && key){//change line & reverse the path(odd)
+                    dir[0]=up; dir[1]=left; dir[2]=down;//queue of leftward
+                    checkOccupied();
+                }else if(maintainQueue(i)){//change line after waste 2s(even)
+                    dir[0]=right; dir[1]=down; dir[2]=left;//queue of downward
+                    checkOccupied();
+                }
+
+            }
+        }else if(dir[i]=up){
+            if(region[x][y-1]=0){
+                y--;
+                move();
+                if(maintainQueue(i) && key){//change line & reverse the path(odd)
+                    dir[0]=right; dir[1]=down; dir[2]=left;//queue of downward
+                    checkOccupied();
+                }else if(maintainQueue(i)){//change line after waste 2s(even)
+                    dir[0]=up; dir[1]=right; dir[2]=down;//queue of rightward strange!!!!!!!!!!!!!
+                    checkOccupied();
+                }
+            }
+        }else if(dir[i]=down){
+            if(region[x][y+1]=0){
+                y++;
+                move();
+                if(maintainQueue(i) && key){//change line & reverse the path(odd)
+                    dir[0]=up; dir[1]=left; dir[2]=down;//queue of leftward
+                    checkOccupied();
+                }else if(maintainQueue(i)){//change line after waste 2s(even)
+                    dir[0]=right; dir[1]=down; dir[2]=left;//queue of downward
+                    checkOccupied();
+                }
+
+            }
+        }else noWay++;
+    }
+    if(noWay==3)
+        //TODO go dir[0]
+}
+
+void move(){
+    region[x][y]=count;
+    count++;
+}
+    
+int maintainQueue(int position){
     int tmp,i;
     tmp = dir[position];
     for(i=position+1;i<4;i++){
         dir[i-1]=dir[i];
     }
     dir[3]=tmp;
+    
+    if(position==2){//position = 2 means two continuous direction were choosen
+        return 1;//1 -> change row or column
+    }else return 0;//0 -> keep going till change line
 }
-
-//void down(int x,int y,int m,int dir,int count,int limitY){
-//    while(limitY<y<m){
-//        switch(dir%4){
-//            case 0:
-//                region[++x][y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            case 1:
-//                region[x][++y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            case 2:
-//                region[--x][y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            case 3:
-//                region[x][++y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            default:
-//                printf("switch error\n");
-//        }
-//    }
-//    count--;
-//    dir--;
-//}
-//
-//void up(int x,int y,int n,int dir,int count,int limitX){
-//    while(limitX<x<n){
-//        switch(dir%4){
-//            case 0:
-//                region[++x][y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            case 1:
-//                region[x][--y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            case 2:
-//                region[--x][y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            case 3:
-//                region[x][--y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            default:
-//                printf("switch error\n");
-//        }
-//    }
-//    count--;
-//    dir--;
-//}
-//
-//void right(int x,int y,int n,int dir,int count,int limitX){
-//    while(limitX<x<n){
-//        switch(dir%4){
-//            case 0:
-//                region[x][--y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            case 1:
-//                region[++x][y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            case 2:
-//                region[x][++y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;				
-//                break;
-//            case 3:
-//                region[++x][y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            default:
-//                printf("switch error\n");
-//        }
-//    }
-//    count--;
-//    dir--;
-//}
-//
-//void left(int x,int y,int n,int dir,int count,int limitX){
-//    while(limitX<y<n){
-//        switch(dir%4){
-//            case 0:
-//                region[x][--y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;				
-//                break;
-//            case 1:
-//                region[--x][y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            case 2:
-//                region[x][++y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;				
-//                break;
-//            case 3:
-//                region[--x][y]=count;
-//                //printf("%d,%d\n",x,y);
-//                dir++;
-//                count++;
-//                break;
-//            default:
-//                printf("switch error\n");
-//        }
-//    }
-//    count--;
-//    dir--;
-//}
